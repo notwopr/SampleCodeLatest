4368ab814f7ceed742678003d54d2e4581567140
notwopr <21273266+notwopr@users.noreply.github.com>
2022-10-27 23:51:27 -0700

    change efs mountpoint

--- machinesettings.py
+++ machinesettings.py
@@ -49,7 +49,7 @@ class Machine():
         },
         'aws-beanstalk': {
             'name': 'awsbeanstalk',
-            'dataroot': '/efs',  # '/efs/CLIMB_DATA'  '/home/ec2-user/environment/EBS',
+            'dataroot': '/efs/CLIMB_DATA',  # '/efs/CLIMB_DATA'  '/home/ec2-user/environment/EBS',
             'numcore_offset': 0
         }
     }
