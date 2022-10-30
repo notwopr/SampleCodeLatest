c2d8fad3d234219771529b5045ee31b52d6e1240
notwopr <21273266+notwopr@users.noreply.github.com>
2022-10-29 19:40:39 -0700

    change root path for beanstalk

--- machinesettings.py
+++ machinesettings.py
@@ -49,7 +49,7 @@ class Machine():
         },
         'aws-beanstalk': {
             'name': 'awsbeanstalk',
-            'dataroot': '/home/ec2-user/CLIMB_DATA',  # '/efs/CLIMB_DATA'  '/home/ec2-user/environment/EBS',
+            'dataroot': '/var/www/html/CLIMB_DATA',  # '/efs/CLIMB_DATA'  '/home/ec2-user/environment/EBS',
             'numcore_offset': 0
         }
     }
