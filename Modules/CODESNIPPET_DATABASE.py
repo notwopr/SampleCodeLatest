44e1fa2f4890da8ecf01b109c6e0375ed67c668b
notwopr <21273266+notwopr@users.noreply.github.com>
2022-11-08 13:40:43 -0800

    remove excess files

--- Modules/CODESNIPPET_DATABASE.py
+++
@@ -1,8 +0,0 @@
-'''
-PURPOSE: BANK OF RANDOM CODE SNIPPETS THAT MAY BE USEFUL.
-'''
-# FILL IN DATE GAPS OF A DATAFRAME WITHOUT CHANGING ITS FORM. NON-DATE COLUMNS ARE FILLED IN WITH NAN
-'''
-newdateindex = pd.date_range(start=mdf['date'].min(), end=mdf['date'].max())
-mdf = mdf.set_index('date').reindex(newdateindex, method='ffill').rename_axis('date').reset_index()
-'''
--- keygen.py
+++
@@ -1,7 +0,0 @@
-from computersettings import computerobject
-from file_functions import readpkl, savetopkl
-
-k = ''
-savetopkl('auth', computerobject.auth, k)
-kr = readpkl('auth', computerobject.auth)
-print(kr==k, kr)
